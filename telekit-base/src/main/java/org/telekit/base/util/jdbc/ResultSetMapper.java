8423338f8018390356d71c457c65a6da283397b8
mkpaz <quizynox@gmail.com>
2021-08-16 08:53:14 +0400

    Refactor ResultSetMapper helpers to avoid NPE

--- telekit-base/src/main/java/org/telekit/base/util/jdbc/ResultSetMapper.java
+++ telekit-base/src/main/java/org/telekit/base/util/jdbc/ResultSetMapper.java
@@ -1,21 +1,37 @@
 package org.telekit.base.util.jdbc;
 
+import org.telekit.base.domain.exception.DatabaseException;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
+// IMPORTANT NOTE:
+// Do not use `rs.getObject(name, class)`, because for some reason it may return null
+// when called from another module, which is probably JDBC driver or JDK bug.
 public interface ResultSetMapper<T> {
 
     T map(ResultSet rs) throws SQLException;
 
     default UUID getUUID(ResultSet rs, String colName) throws SQLException {
-        return rs.getObject(colName, UUID.class);
+        Object o = rs.getObject(colName);
+        if (o instanceof UUID uuid) {
+            return uuid;
+        } else {
+            throw new DatabaseException("Unexpected column type: " + colName);
+        }
     }
 
     default LocalDateTime getLocalDateTime(ResultSet rs, String colName) throws SQLException {
-        OffsetDateTime offsetDateTime = rs.getObject(colName, OffsetDateTime.class);
-        return offsetDateTime != null ? offsetDateTime.toLocalDateTime() : null;
+        Object o = rs.getObject(colName);
+        if (o == null) { return null; }
+
+        if (o instanceof OffsetDateTime odt) {
+            return odt.toLocalDateTime();
+        } else {
+            throw new DatabaseException("Unexpected column type: " + colName);
+        }
     }
 }
