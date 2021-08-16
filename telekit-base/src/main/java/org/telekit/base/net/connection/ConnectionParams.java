420ed1ea4cd5db13e7862bd62ff060ee265d0fbb
mkpaz <quizynox@gmail.com>
2021-08-16 08:13:49 +0400

    Add toUri() method to ConnectionParams

--- telekit-base/src/main/java/org/telekit/base/net/connection/ConnectionParams.java
+++ telekit-base/src/main/java/org/telekit/base/net/connection/ConnectionParams.java
@@ -4,6 +4,8 @@ import org.jetbrains.annotations.Nullable;
 import org.telekit.base.domain.security.Credentials;
 
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 public interface ConnectionParams {
 
@@ -17,5 +19,17 @@ public interface ConnectionParams {
 
     default InetSocketAddress getInetSocketAddress() {
         return new InetSocketAddress(getHost(), getPort());
+    }
+
+    /**
+     * Converts object to the corresponding URI representation.
+     * Note that this method <b>does not</b> store credentials in authority part of the URI.
+     */
+    default URI toUri() {
+        try {
+            return new URI(getScheme().toString(), null, getHost(), getPort(), null, null, null);
+        } catch (URISyntaxException e) {
+            throw new IllegalArgumentException(e);
+        }
     }
 }
