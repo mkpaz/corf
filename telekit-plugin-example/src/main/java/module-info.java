3d9fb9a99e995dafdaf09eab5358e45e7459657d
mkpaz <quizynox@gmail.com>
2021-10-04 10:43:39 +0400

    Remove 'org' prefix in telekit-plugin-example

--- telekit-plugin-example/src/main/java/module-info.java
+++ telekit-plugin-example/src/main/java/module-info.java
@@ -1,4 +1,4 @@
-import org.telekit.base.plugin.Plugin;
+import telekit.base.plugin.Plugin;
 
 module telekit.example {
 
@@ -7,20 +7,20 @@ module telekit.example {
     requires org.jetbrains.annotations;
     requires org.apache.commons.lang3;
 
-    exports org.telekit.example;
-    opens org.telekit.example;
+    exports telekit.example;
+    opens telekit.example;
 
-    exports org.telekit.example.service;
-    opens org.telekit.example.service;
+    exports telekit.example.service;
+    opens telekit.example.service;
 
-    exports org.telekit.example.tools;
-    opens org.telekit.example.tools;
+    exports telekit.example.tools;
+    opens telekit.example.tools;
 
-    provides Plugin with org.telekit.example.ExamplePlugin;
+    provides Plugin with telekit.example.ExamplePlugin;
 
-    // demo package
-    exports org.telekit.example.demo;
-    opens org.telekit.example.demo;
-    exports org.telekit.example.i18n;
-    opens org.telekit.example.i18n;
+    exports telekit.example.demo;
+    opens telekit.example.demo;
+
+    exports telekit.example.i18n;
+    opens telekit.example.i18n;
 }
--- telekit-plugin-example/src/main/java/org/telekit/example/i18n/ExampleMessages.java
+++
@@ -1,9 +0,0 @@
-package org.telekit.example.i18n;
-
-import org.telekit.base.i18n.BaseMessages;
-import org.telekit.controls.i18n.ControlsMessages;
-
-public interface ExampleMessages extends BaseMessages, ControlsMessages {
-
-    String HELLO = "Hello";
-}
