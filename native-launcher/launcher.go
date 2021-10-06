fdf7a6267a90702c3f9057e5837bb735de865d97
mkpaz <quizynox@gmail.com>
2021-10-06 15:39:28 +0400

    Support comments in user.options file

--- native-launcher/launcher.go
+++ native-launcher/launcher.go
@@ -13,25 +13,22 @@ const RESTART_EXIT_CODE = 3
 
 func main() {
   executable, err := os.Executable()
-  if err != nil {
-    log.Fatal(err)
-  }
+  if err != nil { log.Fatal(err) }
   executablePath := filepath.Dir(executable)
 
   var (
-    OPTIONS_FILE = filepath.Join(executablePath, "user.options")
+    USER_OPTIONS_FILE = filepath.Join(executablePath, "user.options")
     JAVA_EXEC = filepath.Join(executablePath, "app", "bin", JAVA_EXEC)
   )
 
-  fileBytes, err := ioutil.ReadFile(OPTIONS_FILE)
-  if err != nil {
-    log.Fatal(err)
-  }
+  fileBytes, err := ioutil.ReadFile(USER_OPTIONS_FILE)
+  if err != nil { log.Fatal(err) }
 
   // gather options
-  optsText := strings.Trim(strings.Replace(string(fileBytes), "\r\n", "\n", -1), "\n");
-  jvmOptions := strings.Split(optsText, "\n")
-  args := append(jvmOptions,
+  optionsText := strings.Trim(strings.Replace(string(fileBytes), "\r\n", "\n", -1), "\n");
+  commentPredicate := func(s string) bool { return !strings.HasPrefix(strings.TrimSpace(s), "#") }
+  userOptions := filter(strings.Split(optionsText, "\n"), commentPredicate)
+  args := append(userOptions,
     "-Dtelekit.app.dir=" + executablePath,
     "-m",
     "telekit.desktop/telekit.desktop.Launcher")
@@ -43,11 +40,20 @@ func main() {
     if err := cmd.Run(); err != nil {
       if exitError, hasError := err.(*exec.ExitError); hasError {
         if exitCode := exitError.ExitCode(); exitCode == RESTART_EXIT_CODE {
-          continue;
+          continue
         }
       }
     }
 
-    break;
+    break
   }
+}
+
+func filter(stringArray []string, predicate func(string) bool) (ret []string) {
+  for _, s := range stringArray {
+    if predicate(s) {
+      ret = append(ret, s)
+    }
+  }
+  return
 }
